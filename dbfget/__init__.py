3fa2d131a18a021a990fc30875a121e4813ce374
Ole Martin Bjørndalen <ombdalen@gmail.com>
2012-08-05 04:49:08 +0200

    Added a comment on the status of the API to README.rst.

--- dbfget/__init__.py
+++ dbfget/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'Ole Martin Bjørndalen'
 __email__ = 'ombdalen@gmail.com'
 __url__ = 'http://nerdly.info/ole/'
 __license__ = 'MIT'
-__version__ = '0.1.0'
+__version__ = '0.1.0x'
 
 from .dbf import DBF as read
 
