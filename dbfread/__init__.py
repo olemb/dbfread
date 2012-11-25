a5e16607c95c025d8ff94169e84489e6946d8619
Ole Martin Bjørndalen <ombdalen@gmail.com>
2012-11-25 23:28:37 +0100

    Cleaned up __init__.py.

--- dbfread/__init__.py
+++ dbfread/__init__.py
@@ -4,11 +4,9 @@ __author__ = 'Ole Martin Bjørndalen'
 __email__ = 'ombdalen@gmail.com'
 __url__ = 'http://nerdly.info/ole/'
 __license__ = 'MIT'
-__version__ = '0.1.0x'
+__version__ = '0.1.0'
 
-from .dbf import Table
-
-read = Table  # Shortcut
+from .dbf import Table as read
 
 class RecObject(object):
     def __init__(self, items):
