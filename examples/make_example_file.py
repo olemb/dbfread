68f28cf97b3541ebbbd6e77e54c84191b523f93a
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-07-26 00:15:54 +0200

    Added a deleted record to the example file.

--- examples/make_example_file.py
+++ examples/make_example_file.py
@@ -14,10 +14,21 @@ db.addField(
 for name, birthdate in (
     ('Alice', (1987, 3, 1)),
     ('Bob', (1980, 11, 12)),
+    ('Deleted Guy', (1979, 12, 22)),
 ):
     rec = db.newRecord()
     rec['NAME'] = name
     rec['BIRTHDATE'] = birthdate
     rec.store()
 
-db.close()
+# Delete the last one of those records.
+rec.delete()
+rec.store()
+
+try:
+    db.close()
+except AttributeError:
+    # This ignores the following error:
+    #     self.memo.flush()
+    # AttributeError: 'NoneType' object has no attribute 'flush'
+    pass
