e04f1cbc077bfc4baea883dddfd323f5b8dd4db4
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-07-26 22:07:45 +0200

    Added example parserclass_debugstring.py.

---
+++ examples/parserclass_debugstring.py
@@ -0,0 +1,17 @@
+"""
+Custom field parser that returns a debug string.
+"""
+import dbfread
+from dbfread.field_parser import FieldParser
+
+class DebugFieldParser(FieldParser):
+    """Field that returns a debug string for each field value."""
+    def field_type_supported(self, field_type):
+        # Everything is supported.
+        return True
+
+    def parse(self, field, value):
+        return '<type={} value={!r}>'.format(field.type, value)
+
+for record in dbfread.open('people.dbf', parserclass=DebugFieldParser):
+    print(record)
