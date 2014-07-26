ce432b645b37bbea6a1440cd3b15f4e5802392fe
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-07-26 16:01:14 +0200

    Added example record_objects.py.

---
+++ examples/record_objects.py
@@ -0,0 +1,12 @@
+"""
+Return records as objects with fields as attributes.
+"""
+import dbfread
+
+class Record(object):
+    def __init__(self, items):
+        for name, value in items:
+            setattr(self, name, value)
+
+for record in dbfread.open('people.dbf', recfactory=Record, lowernames=True):
+    print(record.name)
