aef4556a19c3af61dbfa6d63841b741b247e9096
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-08-02 14:15:42 +0200

    Rewrote make_example_files.py to make it easier to write tests.

--- examples/files/make_example_files.py
+++ examples/files/make_example_files.py
@@ -5,30 +5,35 @@ You need the dbfpy library to run this.
 """
 from dbfpy import dbf
 
-db = dbf.Dbf('people.dbf', new=True)
-db.addField(
-    ('NAME', 'C', 16),
-    ('BIRTHDATE', 'D'),
-    )
+def make_example_file(filename, fields, records, delete_last_record=False):
+    db = dbf.Dbf(filename, new=True)
+    db.addField(*fields)
 
-for name, birthdate in (
-    ('Alice', (1987, 3, 1)),
-    ('Bob', (1980, 11, 12)),
-    ('Deleted Guy', (1979, 12, 22)),
-):
-    rec = db.newRecord()
-    rec['NAME'] = name
-    rec['BIRTHDATE'] = birthdate
-    rec.store()
+    for data in records:
+        record = db.newRecord()
+        for name, value in zip([field[0] for field in fields], data):
+            print(name, value)
+            record[name] = value
+        record.store()
 
-# Delete the last one of those records.
-rec.delete()
-rec.store()
+    if delete_last_record:
+        # Delete the last one of those records.
+        record.delete()
+        record.store()
 
-try:
-    db.close()
-except AttributeError:
-    # This ignores the following error:
-    #     self.memo.flush()
-    # AttributeError: 'NoneType' object has no attribute 'flush'
-    pass
+    try:
+        db.close()
+    except AttributeError:
+        # This ignores the following error:
+        #     self.memo.flush()
+        # AttributeError: 'NoneType' object has no attribute 'flush'
+        pass
+
+
+make_example_file('people.dbf',
+                  [('NAME', 'C', 16),
+                   ('BIRTHDATE', 'D')],
+                  [('Alice', (1987, 3, 1)),
+                   ('Bob', (1980, 11, 12)),
+                   ('Deleted Guy', (1979, 12, 22))],
+                  delete_last_record=True)
