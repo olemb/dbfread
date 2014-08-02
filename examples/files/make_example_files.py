82ad03a4bd39df0ab32a41f4cdf3e38a63847ef3
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-08-02 14:23:54 +0200

    Added example file memotest.dbf/.FPT.

--- examples/files/make_example_files.py
+++ examples/files/make_example_files.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python2
 """
 This creates the example file people.dbf.
 
 You need the dbfpy library to run this.
 """
+from __future__ import print_function
 from dbfpy import dbf
 
 def make_example_file(filename, fields, records, delete_last_record=False):
+    field_names = [field[0] for field in fields]
+
+    print('Creating', filename)
+    print('  Fields:', ', '.join(field_names))
+    print(' ', len(records), 'records')
+
     db = dbf.Dbf(filename, new=True)
     db.addField(*fields)
 
     for data in records:
         record = db.newRecord()
-        for name, value in zip([field[0] for field in fields], data):
-            print(name, value)
+        for name, value in zip(field_names, data):
             record[name] = value
         record.store()
 
@@ -36,4 +43,13 @@ make_example_file('people.dbf',
                   [('Alice', (1987, 3, 1)),
                    ('Bob', (1980, 11, 12)),
                    ('Deleted Guy', (1979, 12, 22))],
+                  delete_last_record=True)
+
+make_example_file('memotest.dbf',
+                  [('NAME', 'C', 16),
+                   ('BIRTHDATE', 'D'),
+                   ('MEMO', 'M')],
+                  [('Alice', (1987, 3, 1), 'Alice memo'),
+                   ('Bob', (1980, 11, 12), 'Bob memo'),
+                   ('Deleted Guy', (1979, 12, 22), 'Deleted guy memo')],
                   delete_last_record=True)
