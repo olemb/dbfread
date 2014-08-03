a507db619a2ca6d26c426deedbe0be2d7aa5cc30
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-08-04 00:23:19 +0200

    Added doc string to __init__.py.

--- dbfread/__init__.py
+++ dbfread/__init__.py
@@ -1,10 +1,21 @@
-# -*- coding: utf-8 -*-
+"""
+Read DBF files with Python.
 
+There is only one function here:
+
+    dbfread.open(filename, **kwargs)
+
+which is an alias for:
+
+    dbfread.DBF(filename, **kwargs)
+"""
 __author__ = 'Ole Martin Bjorndalen'
 __email__ = 'ombdalen@gmail.com'
 __url__ = 'http://nerdly.info/ole/'
 __license__ = 'MIT'
 __version__ = '1.1.1'
+
+print(__doc__)
 
 from .dbf import DBF
 from .dbf import DBF as open  # Alias.
@@ -12,3 +23,4 @@ from .deprecated_dbf import DeprecatedDBF as read
 
 # Prevent splat import.
 __all__ = []
+
