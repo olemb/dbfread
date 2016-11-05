ce409fe4772de8d62e97f36748d488d03387d654
Ole Martin Bjørndalen <ombdalen@gmail.com>
2016-11-05 13:08:05 +0100

    Updated method name to be more descriptive.

--- dbfread/dbf.py
+++ dbfread/dbf.py
@@ -211,7 +211,7 @@ class DBF(object):
             except LookupError as err:
                 self.encoding = 'ascii'
 
-    def _decode(self, data):
+    def _decode_text(self, data):
         return data.decode(self.encoding, errors=self.char_decode_errors)
 
     def _read_field_headers(self, infile):
@@ -232,7 +232,7 @@ class DBF(object):
                 field.decimal_count = 0
 
             # Field name is b'\0' terminated.
-            field.name = self._decode(field.name.split(b'\0')[0])
+            field.name = self._decode_text(field.name.split(b'\0')[0])
             if self.lowernames:
                 field.name = field.name.lower()
 
