de10c58391efe2591425c07039199963b2fa8b54
Rohan Jain <rohan@investwellonline.com>
2020-01-31 18:02:22 +0530

    edited date field parsing to handle NULL values

--- dbfread/field_parser.py
+++ dbfread/field_parser.py
@@ -91,7 +91,7 @@ class FieldParser:
         try:
             return datetime.date(int(data[:4]), int(data[4:6]), int(data[6:8]))
         except ValueError:
-            if data.strip(b' 0') == b'':
+            if ''.join(data.decode().split('\x00')) == '':
                 # A record containing only spaces and/or zeros is
                 # a NULL value.
                 return None
