e00bdfd047ed54d21d30a2d3f77f5d4d6a4fb66c
Ole Martin Bjørndalen <ombdalen@gmail.com>
2020-12-21 08:35:59 +0100

    Fixed python_requires in setup.py (thanks to @shawnbrown).

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     include_package_data=True,
     zip_safe=True,
     install_requires=[],
-    python_requires='>=2.7.*, >=3.6.*',
+    python_requires='>=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     license='MIT',
     classifiers=(
         'Development Status :: 5 - Production/Stable',
